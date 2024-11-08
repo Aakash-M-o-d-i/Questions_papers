a50695e82eace77f5b4490db2830f9ac4f9df6f4
Aakash-M-o-d-i <work.aakash.modi@gmail.com>
2024-11-08 15:31:33 +0530

    Add running text for awareness junior.

--- js/demo.js
+++ js/demo.js
@@ -1,58 +1,12 @@
-// Get the modal
-var modal = document.getElementById("popupContainer");
+// // Optional JavaScript for controlling the speed of the animation
+// const marqueeText = document.getElementById("marquee");
+// let speed = 50; // adjust the speed by changing this value
 
-// Get the <span> element that closes the modal
-var span = document.getElementById("close-btn");
+// function adjustMarqueeSpeed() {
+//   const marqueeWidth = marqueeText.offsetWidth;
+//   const animationDuration = marqueeWidth / speed;
+//   marqueeText.style.animationDuration = `${animationDuration}s`;
+// }
 
-// Get all links
-var links = document.querySelectorAll('a');
-
-// Variable to store the current PDF URL
-var currentPdfUrl = "";
-
-// When the user clicks on any link, open the modal and set the current PDF URL
-links.forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        modal.style.display = "flex"; // Show the modal
-        currentPdfUrl = this.getAttribute('data-pdf'); // Set the current PDF URL
-    });
-});
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-// Handle form submission
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const templateParams = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        subject: document.getElementById('subject').value,
-        message: document.getElementById('message').value
-    };
-
-    emailjs.send('service_v87ru0n', 'template_4x24gft', templateParams)
-        .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-            alert('Message sent successfully!');
-            document.getElementById('contact-form').reset();
-
-            // After registration and email success, close the modal and trigger the PDF download
-            modal.style.display = "none";
-            window.location.href = currentPdfUrl;
-        }, function(error) {
-            console.log('FAILED...', error);
-            alert('Failed to send message. Please try again.');
-        });
-});
+// window.onload = adjustMarqueeSpeed;
+// window.onresize = adjustMarqueeSpeed;
